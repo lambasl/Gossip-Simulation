4a867272db1d6058dd3ed3b36efe2c0d680eda2e
Srinivas Narne <srinivasnarne@outlook.com>
2015-10-03 07:12:04 -0400

    Modification to the Network Topologies
    1. Proposal to change the file structure of different topologies and merge them into one file.
    2. networkGenerator function completed.
    3. randomGenerator function for 3d Grid topologies under progress.

--- src/main/scala/topologies/Topology.scala
+++ src/main/scala/topologies/Topology.scala
@@ -1,10 +1,96 @@
 package topologies
 
 import actors.BaseActor
+import akka.actor._
+import scala.math
 
 /**
  * @author user
  */
-trait Topology {
-  def getRandonNeighbor(actor: BaseActor): BaseActor
+class Topology() extends Actor {
+
+	val rand = new Random()
+	def networkGenerator(numberOfNodes: Int, topology: String, gossipOrPushsum: String) = {
+		var xDimension = 1
+		var yDimension = 1
+		var zDimension = 1
+
+		if (topology = "grid" || "imperfectgrid") {
+			xDimension = math.round(math.cbrt(numberOfNodes))
+			yDimension = xDimension
+			zDimension = xDimension
+		}
+		else {
+			xDimension = numberOfNodes
+		}
+
+		val networkNodes = new Array.ofDim[ActorRef](xDimension.toInt, yDimension.toInt, zDimension.toInt)
+		val networkSystem = ActorSystem("networkSystem")
+
+		for (x <- 0 to xDimension - 1) {
+
+			for (y <- 0 to yDimension - 1) {
+
+				for (z <- 0 to zDimension - 1) {
+					var identity = new Array[Int](3)
+					identity(0) = x
+					identity(1) = y
+					identity(2) = z
+
+					if (gossipOrPushsum == "gossip") {
+						networkNodes(x,y,z) =  networkSystem.actorOf(Props(new GossipActor(this,identity)), name = x.toString + y.toString + z.toString)
+					}
+					else {
+						networkNodes(x,y,z) = networkSystem.actorOf(Props(new PushSumActor(this,identity)), name = x.toString + y.toString + z.toString)
+					}
+				}
+			}
+		}
+
+	}
+
+	def randomNeighborSelector (topology: String, identity: Array[Int], numberOfNodes: Int): Array[Int] = {
+
+		if (topology == "fullnetwork") {
+			var x = rand.nextInt(numberOfNodes)
+			while (x == identity(0)) {
+				 x = rand.nextInt(numberOfNodes)
+			}
+			identity(0) = x
+			identity(1) = 0
+			identity(2) = 0
+			return identity
+		}
+
+		else if (topology == "line") {
+			if (identity(0) == 0) {
+				identity(0) = 1
+			}
+			else if (identity(0) == numberOfNodes - 1) {
+				identity(0) == identity(0) - 1
+			}
+			else {
+				var x= rand.nextInt(2)
+				if (x == 0) {
+					identity(0) = identity(0) - 1
+				}
+				else identity(0) = identity(0) + 1
+			}
+			return identity
+		}
+
+		else if (topology == "grid") {
+			
+		}
+
+		else if (topology == "imperfectgrid") {
+
+		}
+	}
+
+	def receive = {
+
+		case Rumour(topology: Int, numberOfNodes: Int)
+	}
+
 }
