365972e4bfc0cbe0baf569bb23b1c9a47b2feab7
Srinivas Narne <srinivasnarne@outlook.com>
2015-10-03 20:22:53 -0400

    Update Test.scala

--- src/main/scala/main/Test.scala
+++ src/main/scala/main/Test.scala
@@ -2,15 +2,15 @@
 
 package main
 
-import topologies.FullNetwork
- 
+import topologies._
+
 /* @author user
  */
 object Test extends App{
-  def main(args: Array[String]): Unit = {
-    val network = new FullNetwork()
-    network.start()
-    
-  }
-  
+    var numberOfNodes: Int = args(0).toInt
+    val topology: String = args(1).toString
+    val gossipOrPushsum: String = args(2).toString
+    val networkStructure: Array[BaseActor] = Topology.networkGenerator(numberOfNodes, topology, gossipOrPushsum)
+    networkStructure(1,0,0) ! "gossip"
+
 }
